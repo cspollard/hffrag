b38ba2e5fc2465e0628557d224874cba5d270517
Chris Pollard <cspollard@gmail.com>
2022-11-10 18:24:50 +0000

    finally able to read in and match tracks to jets!

--- hffrag.py
+++ hffrag.py
@@ -2,7 +2,18 @@ import uproot
 import awkward
 import numpy
 import matplotlib.figure as figure
+import keras
 
+
+MASKVAL = -999
+MAXTRACKS = 8
+BATCHSIZE = 128
+EPOCHSIZE = 100000
+CLASSWEIGHT = 1
+REGWEIGHT = 1
+MAXEVENTS = 1000
+VALFACTOR = 10
+LR = 1e-3
 
 # this opens up the root file and points to the "CharmAnalysis" tree inside.
 tree = uproot.open("hffrag.root:CharmAnalysis")
@@ -10,6 +21,7 @@ tree = uproot.open("hffrag.root:CharmAnalysis")
 
 # decide which branches of the tree we actually want to look at
 # uncomment as needed!
+# not currently used!
 branches = \
   [ \
 
@@ -23,7 +35,7 @@ branches = \
   # true b-hadron information
   # these b-hadrons are inside the truth jets
   , "AnalysisAntiKt4TruthJets_ghostB_pdgId"
-  # , "AnalysisAntiKt4TruthJets_ghostB_pt"
+  , "AnalysisAntiKt4TruthJets_ghostB_pt"
   # , "AnalysisAntiKt4TruthJets_ghostB_eta"
   # , "AnalysisAntiKt4TruthJets_ghostB_phi"
   # , "AnalysisAntiKt4TruthJets_ghostB_m"
@@ -37,14 +49,133 @@ branches = \
 
 
   # reconstructed track information
-  # , "AnalysisTracks_pt"
-  # , "AnalysisTracks_eta"
-  # , "AnalysisTracks_phi"
+  , "AnalysisTracks_pt"
+  , "AnalysisTracks_eta"
+  , "AnalysisTracks_phi"
   ]
 
 
+# true jet information
+jetfeatures = \
+  [ "AnalysisAntiKt4TruthJets_pt"
+  , "AnalysisAntiKt4TruthJets_eta"
+  , "AnalysisAntiKt4TruthJets_phi"
+  ]
+
+# true b-hadron information
+# these b-hadrons are inside the truth jets
+bhadfeatures = \
+  [ "AnalysisAntiKt4TruthJets_ghostB_pt"
+  , "AnalysisAntiKt4TruthJets_ghostB_eta"
+  , "AnalysisAntiKt4TruthJets_ghostB_phi"
+  # , "AnalysisAntiKt4TruthJets_ghostB_m"
+  ]
+  
+
+# reconstructed track information
+trackfeatures = \
+  [ "AnalysisTracks_pt"
+  , "AnalysisTracks_eta"
+  , "AnalysisTracks_phi"
+  ]
+
 # read in the requested branches from the file
-features = tree.arrays(branches)
+features = tree.arrays(jetfeatures + bhadfeatures + trackfeatures, entry_stop=MAXEVENTS)
+
+
+def matchTracks(jets, trks):
+  jeteta = jets["AnalysisAntiKt4TruthJets_eta"]
+  jetphi = jets["AnalysisAntiKt4TruthJets_phi"]
+
+  trketas = trks["AnalysisTracks_eta"]
+  trkphis = trks["AnalysisTracks_phi"]
+
+  detas = jeteta - trketas
+  dphis = numpy.abs(jetphi - trkphis)
+
+  # deal with delta phis being annoying
+  awkward.where(dphis > numpy.pi, dphis - numpy.pi, dphis)
+
+  return numpy.sqrt(dphis**2 + detas**2) < 0.4
+
+
+def ptetaphi2pxpypz(ptetaphi):
+  pts = ptetaphi[:,0:1]
+  etas = ptetaphi[:,1:2]
+  phis = ptetaphi[:,2:3]
+
+  pxs = pts * numpy.cos(phis)
+  pys = pts * numpy.sin(phis)
+  pzs = pts * numpy.sinh(etas)
+
+  isinf = numpy.isinf(pzs)
+
+  if numpy.any(isinf):
+    print("inf from eta:")
+    print(etas[isinf])
+    raise ValueError("infinity from sinh(eta)")
+
+  return numpy.concatenate([pxs, pys, pzs], axis=1)
+
+
+def ptetaphi2pxpypz2(ptetaphi):
+  pts = ptetaphi[:,:,0:1]
+  etas = ptetaphi[:,:,1:2]
+  phis = ptetaphi[:,:,2:3]
+
+  mask = pts == MASKVAL
+  pxs = numpy.where(mask, pts, pts * numpy.cos(phis))
+  pys = numpy.where(mask, pts, pts * numpy.sin(phis))
+  pzs = numpy.where(mask, pts, pts * numpy.sinh(etas))
+
+  isinf = numpy.isinf(pzs)
+
+  if numpy.any(isinf):
+    print("inf from eta:")
+    print(etas[isinf])
+    raise ValueError("infinity from sinh(eta)")
+
+  return numpy.concatenate([pxs, pys, pzs], axis=2)
+
+
+# pads inputs with nans up to the given maxsize
+def pad(xs, maxsize):
+  ys = \
+    awkward.fill_none \
+    ( awkward.pad_none(xs, maxsize, axis=1, clip=True)
+    , MASKVAL
+    )[:,:maxsize]
+
+  return awkward.to_regular(ys, axis=1)
+
+
+def flatten1(xs, maxsize=-1):
+  ys = {}
+  for field in xs.fields:
+    zs = xs[field]
+    if maxsize > 0:
+      zs = pad(zs, maxsize)
+    ys[field] = zs
+
+  return awkward.zip(ys)
+
+
+
+from numpy.lib.recfunctions import structured_to_unstructured
+events = \
+  features[awkward.sum(features["AnalysisAntiKt4TruthJets_pt"] > 25000, axis=1) > 0]
+
+jets = events[jetfeatures][:,0]
+tracks = events[trackfeatures]
+
+matchedtracks = tracks[matchTracks(jets, tracks)]
+matchedtracks = flatten1(matchedtracks, MAXTRACKS)
+
+jets = structured_to_unstructured(jets)
+matchedtracks = structured_to_unstructured(matchedtracks)
+
+jets = ptetaphi2pxpypz(jets).to_numpy()
+tracks = ptetaphi2pxpypz2(matchedtracks.to_numpy())
 
 
 # returns a fixed set of bin edges
@@ -91,44 +222,59 @@ def binneddensity(xs, binning, label=None, xlabel=None, ylabel="binned probabili
   plt.set_xlabel(xlabel)
   plt.set_ylabel(ylabel)
 
-  return fig
+  return fig/exit
 
 
-# this creates a histogram of the truth jet pTs.
-truthjets = features["AnalysisAntiKt4TruthJets_pt"] 
+# here is where the training starts.
 
-fig = \
-  binneddensity \
-  ( truthjets[:,0] # the first jet in each event
-  , fixedbinning(0, 200000, 50) # 50 bins from 0 to 200000 MeV
-  , xlabel="first truth jet $p_T$ [MeV]"
+tracklayers = [ 32 , 32 , 32 , 32 , 32 ]
+jetlayers = [ 64 , 64 , 64 , 64 , 64 ]
+
+def buildModel(tlayers, jlayers, ntargets):
+  inputs = layers.Input(shape=(None, tlayers[0]))
+
+  outputs = inputs
+  outputs = layers.Masking(mask_value=MASKVAL)(outputs)
+
+  for nodes in tlayers[:-1]:
+    outputs = layers.TimeDistributed(layers.Dense(nodes, activation='relu'))(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+
+  outputs = layers.TimeDistributed(layers.Dense(tlayers[-1], activation='softmax'))(outputs)
+  outputs = Sum()(outputs)
+
+  for nodes in jlayers:
+    outputs = layers.Dense(nodes, activation='relu')(outputs)
+    outputs = layers.BatchNormalization()(outputs)
+
+
+  # we only want one output -- for now!
+  outmean = layers.Dense(ntargets)(outputs)
+  outcov = layers.Dense(ntargets*(ntargets+1)//2)(outputs)
+
+  return \
+    keras.Model \
+    ( inputs = inputs
+    , outputs = { "means" : outmean, "covs" : outcov }
+    )
+
+
+# TODO
+# this ignores any dimension beyond the first!
+def LogNormal1D(true, meanscovs):
+  means = meanscovs["means"]
+  covs = meanscovs["covs"]
+
+  return tfd.MultivariateNormalTriL(loc=means[:,:1], scale_tril=covs[:,:1])
+
+
+model = buildModel([len(trackfeatures)] + tracklayers, jetlayers)
+
+model.summary()
+
+model.compile \
+  ( loss = LogNormal1D
+  , optimizer = keras.optimizers.Adam(learning_rate=LR)
   )
 
-fig.savefig("truthjet-pt.png")
-
-
-# this counts the number of b-hadron identification numbers and prints out the
-# counts.
-# these are defined in the PDG (but no need to worry about the details for now!)
-# https://pdg.lbl.gov/2007/reviews/montecarlorpp.pdf
-
-
-truthbhadronsid = \
-  awkward.flatten \
-  ( features["AnalysisAntiKt4TruthJets_ghostB_pdgId"]
-  , axis=None
-  )
-
-# loop over all the b-hadrons associated to jets
-counts = {}
-for bhadid in truthbhadronsid:
-  if bhadid in counts:
-    counts[bhadid] += 1
-  else:
-    counts[bhadid] = 1
-
-print()
-for bhadid in counts:
-  print("PDG ID:", bhadid)
-  print("number of b-hadrons:", counts[bhadid])
-  print()
+model.fit()
